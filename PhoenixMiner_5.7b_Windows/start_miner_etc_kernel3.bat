db0004e5ba7284f0ce78bbb6e3155d6f4abd81f6
LucasTheisen31 <78063228+LucasTheisen31@users.noreply.github.com>
2021-09-08 12:00:23 -0300

    Add files via upload

---
+++ PhoenixMiner_5.7b_Windows/start_miner_etc_kernel3.bat
@@ -0,0 +1,13 @@
+REM
+REM Example bat file for starting PhoenixMiner.exe to mine ETC
+REM
+
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+
+PhoenixMiner.exe -pool ssl://eth-br.flexpool.io:5555 -pool2 ssl://eth-us-east.flexpool.io:5555 -wal 0x42F2DA498a7a852A2918B2B7291E0178BEb05E0A.PhoenixMiner5.7b -clkernel 3
+pause
+
